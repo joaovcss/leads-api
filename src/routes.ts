0019e79e0418cd14c3a65caf2b4288777ee76206
João Victor <joaovictorcarvalhosilva29@gmail.com>
2025-12-14 16:12:45 -0300

    added campaigns routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { leadsController } from "./controllers/lead-controller.ts"
 import { groupsController } from "./controllers/group-controller.ts"
+import { campaignController } from "./controllers/campaign-controller.ts"
 
 const router = Router()
 const LeadsController = new leadsController()
 const GroupController = new groupsController()
+const CampaingController = new campaignController()
 
 //Leads routes
 router.get("/leads", LeadsController.index)
@@ -19,6 +21,13 @@ router.get("/groups/:id", GroupController.show)
 router.post("/groups", GroupController.create)
 router.put("/groups/:id", GroupController.update)
 router.delete("/groups/:id", GroupController.delete)
+
+//Campaings routes
+router.get("/campaigns", CampaingController.index)
+router.get("/campaigns/:id", CampaingController.show)
+router.post("/campaigns", CampaingController.create)
+router.put("/campaigns/:id", CampaingController.update)
+router.delete("/campaigns/:id", CampaingController.delete)
 
 //Server status check
 router.get("/status", async (req, res, next) => {
