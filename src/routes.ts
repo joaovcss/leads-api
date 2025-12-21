97eb3e0c0555e582f5c4654937c4b4c8a0fa85e0
João Victor <joaovictorcarvalhosilva29@gmail.com>
2025-12-21 00:16:46 -0300

    added group leads routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,14 @@ import { leadsController } from "./controllers/lead-controller.ts"
 import { groupsController } from "./controllers/group-controller.ts"
 import { campaignController } from "./controllers/campaign-controller.ts"
 import { campaignLeadsController } from "./controllers/campaign-leads-controller.ts"
+import { groupLeadsController } from "./controllers/group-leads-controller.ts"
 
 const router = Router()
 const LeadsController = new leadsController()
 const GroupController = new groupsController()
 const CampaingController = new campaignController()
 const CampaigLeadsController = new campaignLeadsController()
+const GroupLeadsController = new groupLeadsController()
 
 //Leads routes
 router.get("/leads", LeadsController.index)
@@ -23,6 +25,11 @@ router.get("/groups/:id", GroupController.show)
 router.post("/groups", GroupController.create)
 router.put("/groups/:id", GroupController.update)
 router.delete("/groups/:id", GroupController.delete)
+
+// Leads management in groups
+router.get("/groups/:groupId/leads", GroupLeadsController.showLeads)
+router.post("/groups/:groupId/leads/" , GroupLeadsController.addLead)
+router.delete("/groups/:groupdId/leads/:leadId", GroupLeadsController.removeLead)
 
 //Campaings routes
 router.get("/campaigns", CampaingController.index)
