4ca73a39a54e0c92dca48f6c85ddb682b342f5a4
João Victor <joaovictorcarvalhosilva29@gmail.com>
2025-12-21 00:15:10 -0300

    created group leads request schemes

--- src/controllers/schemes/groups-request-schemes.ts
+++ src/controllers/schemes/groups-request-schemes.ts
@@ -9,3 +9,15 @@ export const UpdateGroupRequestSchema = z.object({
   name: z.string().optional(),
   description: z.string().optional()
 })
+
+export const GetGroupLeadsRequestSchema = z.object({
+  page: z.string().optional(),
+  pageSize: z.string().optional(),
+  name: z.string().optional(),
+  sortBy: z.enum(["name"]).optional(),
+  order: z.enum([ "asc", "desc" ]).optional()
+})
+
+export const AddGroupLeadsRequestSchema = z.object({
+  leadId: z.coerce.number()
+})
