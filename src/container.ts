c6c39dd97ded12cac69bda45930f3559c3b16c7a
João Victor <joaovictorcarvalhosilva29@gmail.com>
2025-12-27 17:08:22 -0300

    container update

--- src/container.ts
+++ src/container.ts
@@ -4,11 +4,14 @@ import { campaignController } from "./controllers/campaign-controller.ts"
 import { campaignLeadsController } from "./controllers/campaign-leads-controller.ts"
 import { groupLeadsController } from "./controllers/group-leads-controller.ts"
 import { PrismaLeadRepository } from "./repositories/prisma/prisma-lead-repository.ts"
+import { PrismaGroupsRepository } from "./repositories/prisma/prisma-groups-repository.ts"
 
 export const leadsRepository = new PrismaLeadRepository()
 export const LeadsController = new leadsController(leadsRepository)
 
-export const GroupController = new groupsController()
+export const groupsRepository = new PrismaGroupsRepository()
+export const GroupController = new groupsController(groupsRepository)
+
 export const CampaingController = new campaignController()
 export const CampaigLeadsController = new campaignLeadsController()
 export const GroupLeadsController = new groupLeadsController()
